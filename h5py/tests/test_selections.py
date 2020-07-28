5bf7754c4bb0b016854e6fe4238545a14cb93a5e
chenguoping <chenguopingdota@163.com>
2020-07-28 20:09:22 +0800

    update test_selections.py

--- h5py/tests/test_selections.py
+++ h5py/tests/test_selections.py
@@ -65,6 +65,9 @@ class TestTypeGeneration(BaseSelection):
         self.assertEqual(out, np.dtype('i'))
         self.assertEqual(format, np.dtype( [('a','i')] ))
 
+        # Field does not apear in named typed
+        with self.assertRaises(ValueError):
+            out, format = sel2.read_dtypes(dt, ('j', 'k'))
 
 class TestScalarSliceRules(BaseSelection):
 
@@ -85,10 +88,60 @@ class TestScalarSliceRules(BaseSelection):
         with self.assertRaises(ValueError):
             shape, selection = sel2.read_selections_scalar(self.dsid, (1,))
 
+        dsid = self.f.create_dataset('y', (1,)).id
+        with self.assertRaises(RuntimeError):
+            shape, selection = sel2.read_selections_scalar(dsid, (1,))
+
 class TestSelection(BaseSelection):
+    """ High-level routes to generate a selection
+    """
+
     def test_selection(self):
-        #args = (h5py.RegionReference,)
-        #dset1 = self.f.create_dataset('dset', (10,10))
-        #regref = dset1.regionref[...]
-        #select = sel.select((10,), args, dset1)
-        pass
+        dset = self.f.create_dataset('dset', (100,100))
+        regref = dset.regionref[0:100, 0:100]
+
+        # args is numpy.ndarray, return a FancySelection
+        st = sel.select((10,), np.array([1,2,3], dtype='b'), dset)
+        self.assertIsInstance(st, sel.FancySelection)
+
+        self.assertEqual(st.mshape, (3,100))
+        self.assertEqual(st.array_shape, ())
+
+        # expand shape
+        self.assertEqual(st.expand_shape(()), ())
+        with self.assertRaises(TypeError):
+            st.expand_shape((4,100))
+
+        # broadcast in FancySelection
+        st.broadcast((5,100))
+
+        # args is int, return a SimpleSelection
+        st2 = sel.select((10,), 1, dset)
+        self.assertIsInstance(st2, sel.SimpleSelection)
+
+        self.assertEqual(st2.mshape, (1,100))
+        self.assertEqual(st2.array_shape, (100,))
+
+        # expand shape
+        self.assertEqual(st2.expand_shape(()), (1,1))
+        with self.assertRaises(TypeError):
+            st2.expand_shape((4,100))
+
+        # broadcast in SimpleSelection
+        st2.broadcast((5,100))
+
+        # args is RegionReference, but dataset is None
+        with self.assertRaises(TypeError):
+            sel.select((100,), regref, None)
+
+        # args is RegionReference, but its shape doesn't match dataset shape
+        with self.assertRaises(TypeError):
+            sel.select((100,), regref, dset)
+
+        # args is a Selection instance, return a FancySelection
+        st3 = sel.select((100,100), st, dset)
+        self.assertIsInstance(st, sel.FancySelection)
+
+        # args is a single Selection instance, but args shape doesn't match Shape
+        with self.assertRaises(TypeError):
+            sel.select((100,), st, dset)
